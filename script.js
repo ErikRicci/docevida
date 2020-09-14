9a687cd64f502394dcb9d1374ddb018b61e0cd23
Erik <erikdavinci@gmail.com>
2020-09-14 07:44:37 -0300

    Muitas coisas mudaram. Testando agora um sistema de páginas integradas à uma só...

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+//Action
+var page = "procurar";
+
+$(document).ready(function () {
+    page = "procurar";
+    if (page == "procurar")
+    {
+        document.getElementById('body2').innerHTML = procurarProdutos;
+        document.getElementById('carrinho-button').innerHTML = comprarProdutos;
+    }
+});
+
 //PEDIDO PELO WHATSAPP
 var whatsappEnviar = () => {
 
-    var nome = document.getElementById('contact-form').nome_cliente.value;
-    var pedido = document.getElementById('contact-form').pedido.value;
+    window.teste = "TESTADO";
+
+    var nome = document.getElementById('name').value;
+    var pedido = document.getElementById('pedido').value;
 
     var texto = 'Olá,meu nome é ' + nome + '! Gostaria de fazer um pedido.PEDIDO: ' + pedido;
     var href = 'https://api.whatsapp.com/send?phone=5519982492037&text=' + texto + '&lang=pt_br';
 
-    window.location.href = href;
+    //window.location.href = href;
 }
 
-//AJAX PARA TER RESPOSTAS
-var AJAXemJS = () => {
-    window.location.href = 'test.php';
+var getCarrinho = () => {
+    if (page == "procurar") {
+        page = "finalizar";
+        document.getElementById('body2').innerHTML = finalPedido;
+        document.getElementById('carrinho-button').innerHTML = totalProdutos;
+    }
+    else {
+        page = "procurar";
+        document.getElementById('body2').innerHTML = procurarProdutos;
+        document.getElementById('carrinho-button').innerHTML = comprarProdutos;
+    }
+
 }
